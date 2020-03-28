fdd493b16b5a20d32db7ca3378e6640010ed5e2a
Mitchell Katz <Sk1er@Sk1er.club>
2020-03-28 18:06:11 -0400

    Fix import

--- src/main/java/club/sk1er/mods/core/universal/UniversalGraphicsHandler.java
+++ src/main/java/club/sk1er/mods/core/universal/UniversalGraphicsHandler.java
@@ -1,7 +1,7 @@
 package club.sk1er.mods.core.universal;
 
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.OpenGlHelper;
+
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.texture.DynamicTexture;
@@ -20,6 +20,8 @@ import java.util.List;
 //$$ import java.nio.ByteBuffer;
 //$$ import net.minecraft.client.renderer.texture.NativeImage;
 //#else
+import net.minecraft.client.renderer.OpenGlHelper;
+
 //#endif
 
 public class UniversalGraphicsHandler {
