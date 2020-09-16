6a15280e29f238bcd1386fc9bc896c221d10a49a
Matthew Olsson <matthewcolsson@gmail.com>
2020-09-16 13:51:18 -0700

    UniversalKeyboard: Use version-correct key mappings

--- src/main/java/club/sk1er/mods/core/universal/UniversalKeyboard.java
+++ src/main/java/club/sk1er/mods/core/universal/UniversalKeyboard.java
@@ -2,32 +2,49 @@ package club.sk1er.mods.core.universal;
 
 //#if FABRIC
 //$$ import net.minecraft.client.util.InputUtil;
+//$$ import org.lwjgl.glfw.GLFW;
 //#else
 //$$ import net.minecraft.client.Minecraft;
 //#if MC<=11202
 import org.lwjgl.input.Keyboard;
 //#else
+//$$ import org.lwjgl.glfw.GLFW;
 //$$ import net.minecraft.client.util.InputMappings;
 //#endif
 //#endif
 
-
 public class UniversalKeyboard {
-    // These are taken from LWJGL2's Keyboard class for compatibility between MC versions.
-    public static final int KEY_LMETA = 0xDB;
-    public static final int KEY_RMETA = 0xDC;
-    public static final int KEY_LCONTROL = 0x1D;
-    public static final int KEY_RCONTROL = 0x9D;
-    public static final int KEY_LSHIFT = 0x2A;
-    public static final int KEY_RSHIFT = 0x36;
-    public static final int KEY_LMENU = 0x38;
-    public static final int KEY_RMENU = 0xB8;
-    public static final int KEY_X = 0x2D;
-    public static final int KEY_V = 0x2F;
-    public static final int KEY_C = 0x2E;
-    public static final int KEY_A = 0x1E;
-    public static final int KEY_Z = 0x2C;
-    public static final int KEY_Y = 0x15;
+    //#if MC>=11502
+    //$$ public static final int KEY_LMETA = GLFW.GLFW_KEY_LEFT_SUPER; // TODO: Correct?
+    //$$ public static final int KEY_RMETA = GLFW.GLFW_KEY_RIGHT_SUPER; // TODO: Correct?
+    //$$ public static final int KEY_LCONTROL = GLFW.GLFW_KEY_LEFT_CONTROL;
+    //$$ public static final int KEY_RCONTROL = GLFW.GLFW_KEY_RIGHT_CONTROL;
+    //$$ public static final int KEY_LSHIFT = GLFW.GLFW_KEY_LEFT_SHIFT;
+    //$$ public static final int KEY_RSHIFT = GLFW.GLFW_KEY_RIGHT_SHIFT;
+    //$$ public static final int KEY_LMENU = GLFW.GLFW_KEY_LEFT_ALT;
+    //$$ public static final int KEY_RMENU = GLFW.GLFW_KEY_RIGHT_ALT;
+    //$$ public static final int KEY_X = GLFW.GLFW_KEY_X;
+    //$$ public static final int KEY_V = GLFW.GLFW_KEY_V;
+    //$$ public static final int KEY_C = GLFW.GLFW_KEY_C;
+    //$$ public static final int KEY_A = GLFW.GLFW_KEY_A;
+    //$$ public static final int KEY_Z = GLFW.GLFW_KEY_Z;
+    //$$ public static final int KEY_Y = GLFW.GLFW_KEY_Y;
+    //#else
+    public static final int KEY_LMETA = Keyboard.KEY_LMETA;
+    public static final int KEY_RMETA = Keyboard.KEY_RMETA;
+    public static final int KEY_LCONTROL = Keyboard.KEY_LCONTROL;
+    public static final int KEY_RCONTROL = Keyboard.KEY_RCONTROL;
+    public static final int KEY_LSHIFT = Keyboard.KEY_LSHIFT;
+    public static final int KEY_RSHIFT = Keyboard.KEY_RSHIFT;
+    public static final int KEY_LMENU = Keyboard.KEY_LMENU;
+    public static final int KEY_RMENU = Keyboard.KEY_RMENU;
+    public static final int KEY_X = Keyboard.KEY_X;
+    public static final int KEY_V = Keyboard.KEY_V;
+    public static final int KEY_C = Keyboard.KEY_C;
+    public static final int KEY_A = Keyboard.KEY_A;
+    public static final int KEY_Z = Keyboard.KEY_Z;
+    public static final int KEY_Y = Keyboard.KEY_Y;
+    //#endif
 
     public static void enableRepeatEvents(boolean enable) {
         //#if FABRIC
