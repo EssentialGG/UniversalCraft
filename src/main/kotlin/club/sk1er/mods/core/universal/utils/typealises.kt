e9b67db309fffee39b7c5392335d1fa705ba09ed
Matthew Olsson <matthewcolsson@gmail.com>
2020-12-16 11:16:18 -0800

    typealises: Add MCMainMenuScreen alias

--- src/main/kotlin/club/sk1er/mods/core/universal/utils/typealises.kt
+++ src/main/kotlin/club/sk1er/mods/core/universal/utils/typealises.kt
@@ -9,6 +9,7 @@ package club.sk1er.mods.core.universal.utils
 //$$ typealias MCEntityPlayerSP = net.minecraft.client.network.ClientPlayerEntity
 //$$ typealias MCClientNetworkHandler = net.minecraft.client.network.ClientPlayNetworkHandler
 //$$ typealias MCSChatPacket = net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket
+//$$ typealias MCMainMenuScreen = net.minecraft.client.gui.screen.TitleScreen
 //#else
 typealias MCMinecraft = net.minecraft.client.Minecraft
 typealias MCFontRenderer = net.minecraft.client.gui.FontRenderer
@@ -20,11 +21,13 @@ typealias MCFontRenderer = net.minecraft.client.gui.FontRenderer
 //$$ typealias MCScreen = net.minecraft.client.gui.AbstractGui
 //$$ typealias MCClientNetworkHandler = net.minecraft.client.network.play.ClientPlayNetHandler
 //$$ typealias MCSChatPacket = net.minecraft.network.play.server.SChatPacket
+//$$ typealias MCMainMenuScreen = net.minecraft.client.gui.screen.MainMenuScreen
 //#else
 typealias MCWorld = net.minecraft.client.multiplayer.WorldClient
 typealias MCEntityPlayerSP = net.minecraft.client.entity.EntityPlayerSP
 typealias MCScreen = net.minecraft.client.gui.GuiScreen
 typealias MCChatScreen = net.minecraft.client.gui.GuiNewChat
+typealias MCMainMenuScreen = net.minecraft.client.gui.GuiMainMenu
 typealias MCClientNetworkHandler = net.minecraft.client.network.NetHandlerPlayClient
 
 //#if MC>=11202
