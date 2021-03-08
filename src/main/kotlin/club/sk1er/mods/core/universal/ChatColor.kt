3cd3b1ec0260315c1ef438d61277043fecf85761
Matthew Olsson <matthewcolsson@gmail.com>
2021-03-07 21:15:50 -0800

    ChatColor: Convert to Kotlin

---
+++ src/main/kotlin/club/sk1er/mods/core/universal/ChatColor.kt
@@ -0,0 +1,45 @@
+package club.sk1er.mods.core.universal
+
+enum class ChatColor(val char: Char, val isFormat: Boolean = true) {
+    BLACK('0'),
+    DARK_BLUE('1'),
+    DARK_GREEN('2'),
+    DARK_AQUA('3'),
+    DARK_RED('4'),
+    DARK_PURPLE('5'),
+    GOLD('6'),
+    GRAY('7'),
+    DARK_GRAY('8'),
+    BLUE('9'),
+    GREEN('a'),
+    AQUA('b'),
+    RED('c'),
+    LIGHT_PURPLE('d'),
+    YELLOW('e'),
+    WHITE('f'),
+    MAGIC('k', true),
+    BOLD('l', true),
+    STRIKETHROUGH('m', true),
+    UNDERLINE('n', true),
+    ITALIC('o', true),
+    RESET('r');
+
+    override fun toString() = "$COLOR_CHAR$char"
+
+    fun isColor() = !isFormat && this != RESET
+
+    companion object {
+        const val COLOR_CHAR = '\u00a7'
+
+        fun translateAlternateColorCodes(altColorChar: Char, textToTranslate: String): String {
+            val b = textToTranslate.toCharArray()
+            for (i in 0 until b.size - 1) {
+                if (b[i] == altColorChar && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(b[i + 1]) > -1) {
+                    b[i] = COLOR_CHAR
+                    b[i + 1] = Character.toLowerCase(b[i + 1])
+                }
+            }
+            return String(b)
+        }
+    }
+}
