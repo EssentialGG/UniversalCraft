4288d6b07cddfadca51573945bcdfe100e846ae6
asbyth <cyronize@gmail.com>
2021-06-12 06:58:19 -0400

    GuiScale: Create a way to get the recommended scale for the screen size

--- src/main/kotlin/gg/essential/universal/GuiScale.kt
+++ src/main/kotlin/gg/essential/universal/GuiScale.kt
@@ -1,5 +1,7 @@
 package gg.essential.universal
 
+import kotlin.math.min
+
 enum class GuiScale {
     Auto,
     Small,
@@ -10,5 +12,13 @@ enum class GuiScale {
     companion object {
         @JvmStatic
         fun fromNumber(number: Int): GuiScale = values()[number]
+
+        @JvmStatic
+        fun scaleForScreenSize(): GuiScale {
+            val width = UResolution.windowWidth
+            val height = UResolution.windowHeight
+            val step = 700
+            return fromNumber(min((width / step).coerceIn(1, 4), height / (step / 16 * 9).coerceIn(1, 4)))
+        }
     }
 }
