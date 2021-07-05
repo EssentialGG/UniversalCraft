181a094c4a7dd8408905793cecea65270c8b64db
Jonas Herzig <jonas@spark-squared.com>
2021-07-05 13:38:56 +0200

    Build: Temporarily disable Fabric sub-project
    Eclipse did a breaking change (bump from Java 8 to 11) without a major version
    bump and is using fuzzy versions, so we automatically get the incompatible one
    and CI breaks.
    As a workaround, this commit disables the fabric sub-project until we switch to
    Java 16 ourselves (that is, until we merge the `next` branch).

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,8 +25,7 @@ listOf(
     "1.8.9",
     "1.12.2",
     "1.15.2",
-    "1.16.2",
-    "1.16.2-fabric"
+    "1.16.2"
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
