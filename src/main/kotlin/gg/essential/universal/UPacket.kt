bcd8e9e9d26b58df7dedd5497c9ce62fca1436be
Jonas Herzig <jonas@spark-squared.com>
2021-09-18 17:50:49 +0200

    UPacket: DRY preprocessor statements

--- src/main/kotlin/gg/essential/universal/UPacket.kt
+++ src/main/kotlin/gg/essential/universal/UPacket.kt
@@ -9,32 +9,33 @@ import gg.essential.universal.wrappers.message.UTextComponent
 
 //#if MC>=11202
 //$$ import net.minecraft.util.text.ChatType
+//$$
+private object ChatType {
+    const val CHAT: Byte = 0
+    const val GAME_INFO: Byte = 2
+}
 //#endif
 
 object UPacket {
     @JvmStatic
     fun sendChatMessage(message: UTextComponent) {
-        //#if MC>=11602
-        //$$ UMinecraft.getNetHandler()!!.handleChat(MCSChatPacket(message, ChatType.CHAT, UPlayer.getUUID()))
-        //#elseif MC>=11502
-        //$$ UMinecraft.getNetHandler()!!.handleChat(MCSChatPacket(message, ChatType.CHAT))
-        //#elseif MC>=11202
-        //$$ UMinecraft.getNetHandler()!!.handleChat(MCSChatPacket(message, ChatType.CHAT))
-        //#else
-        UMinecraft.getNetHandler()!!.handleChat(MCSChatPacket(message, 0))
-        //#endif
+        UMinecraft.getNetHandler()!!.handleChat(MCSChatPacket(
+            message,
+            ChatType.CHAT,
+            //#if MC>=11600
+            //$$ UPlayer.getUUID(),
+            //#endif
+        ))
     }
     
     @JvmStatic
     fun sendActionBarMessage(message: UTextComponent) {
-        //#if MC>=11602
-        //$$ UMinecraft.getNetHandler()!!.handleChat(MCSChatPacket(message, ChatType.GAME_INFO, UPlayer.getUUID()))
-        //#elseif MC>=11502
-        //$$ UMinecraft.getNetHandler()!!.handleChat(MCSChatPacket(message, ChatType.GAME_INFO))
-        //#elseif MC>=11202
-        //$$ UMinecraft.getNetHandler()!!.handleChat(MCSChatPacket(message, ChatType.GAME_INFO))
-        //#else
-        UMinecraft.getNetHandler()!!.handleChat(MCSChatPacket(message, 2))
-        //#endif
+        UMinecraft.getNetHandler()!!.handleChat(MCSChatPacket(
+            message,
+            ChatType.GAME_INFO,
+            //#if MC>=11600
+            //$$ UPlayer.getUUID(),
+            //#endif
+        ))
     }
 }
