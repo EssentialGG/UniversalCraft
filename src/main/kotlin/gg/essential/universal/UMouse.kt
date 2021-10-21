4ee7cb01e6ca3b3e9b9acb8890e7e9d27d65a868
Jonas Herzig <jonas@spark-squared.com>
2021-10-21 15:37:05 +0200

    UMouse: Provide methods with consistent orientation
    Cause the current Y methods are flipped in 1.12.2 and below which make it way to
    easy to use it incorrectly without noticing.

--- src/main/kotlin/gg/essential/universal/UMouse.kt
+++ src/main/kotlin/gg/essential/universal/UMouse.kt
@@ -7,7 +7,40 @@ import org.lwjgl.input.Mouse
 import kotlin.math.max
 
 object UMouse {
+    object Raw {
+        @JvmStatic
+        val x: Double
+            get() {
+                //#if MC>=11502
+                //$$ return UMinecraft.getMinecraft().mouseHelper.mouseX
+                //#else
+                return Mouse.getX().toDouble()
+                //#endif
+            }
+
+        @JvmStatic
+        val y: Double
+            get() {
+                //#if MC>=11400
+                //$$ return UMinecraft.getMinecraft().mouseHelper.mouseY
+                //#else
+                return UResolution.windowHeight - Mouse.getY().toDouble() - 1
+                //#endif
+            }
+    }
+
+    object Scaled {
+        @JvmStatic
+        val x: Double
+            get() = Raw.x * UResolution.scaledWidth / max(1, UResolution.windowWidth)
+
+        @JvmStatic
+        val y: Double
+            get() = Raw.y * UResolution.scaledHeight / max(1, UResolution.windowHeight)
+    }
+
     @JvmStatic
+    @Deprecated("Orientation is different between Minecraft versions.", replaceWith = ReplaceWith("UMouse.Raw.x"))
     fun getTrueX(): Double {
         //#if MC>=11502
         //$$ return UMinecraft.getMinecraft().mouseHelper.mouseX
@@ -17,11 +50,14 @@ object UMouse {
     }
 
     @JvmStatic
+    @Deprecated("Orientation is different between Minecraft versions.", replaceWith = ReplaceWith("UMouse.Scaled.x"))
+    @Suppress("DEPRECATION")
     fun getScaledX(): Double {
         return getTrueX() * UResolution.scaledWidth / max(1, UResolution.windowWidth)
     }
 
     @JvmStatic
+    @Deprecated("Orientation is different between Minecraft versions.", replaceWith = ReplaceWith("UMouse.Raw.y"))
     fun getTrueY(): Double {
         //#if MC>=11502
         //$$ return UMinecraft.getMinecraft().mouseHelper.mouseY
@@ -31,6 +67,8 @@ object UMouse {
     }
 
     @JvmStatic
+    @Deprecated("Orientation is different between Minecraft versions.", replaceWith = ReplaceWith("UMouse.Scaled.y"))
+    @Suppress("DEPRECATION")
     fun getScaledY(): Double {
         return getTrueY() * UResolution.scaledHeight / max(1, UResolution.windowHeight)
     }
