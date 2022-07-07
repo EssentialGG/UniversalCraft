0e7e9d3adeb34771f5baac34fd11803117827cb3
Jonas Herzig <jonas@spark-squared.com>
2022-07-07 18:10:00 +0200

    Build: Add Forge 1.19

--- root.gradle.kts
+++ root.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 version = versionFromBuildIdAndBranch()
 
 preprocess {
+    val forge11900 = createNode("1.19-forge", 11900, "srg")
     val fabric11900 = createNode("1.19-fabric", 11900, "yarn")
     val forge11801 = createNode("1.18.1-forge", 11801, "srg")
     val fabric11801 = createNode("1.18.1-fabric", 11801, "yarn")
@@ -20,6 +21,7 @@ preprocess {
     val forge11202 = createNode("1.12.2-forge", 11202, "srg")
     val forge10809 = createNode("1.8.9-forge", 10809, "srg")
 
+    forge11900.link(fabric11900)
     fabric11900.link(fabric11801)
     forge11801.link(fabric11801)
     fabric11801.link(fabric11701)
