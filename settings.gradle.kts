2423ec054a516efafb38655c92b29287e8fbb0b8
Jonas Herzig <jonas@spark-squared.com>
2023-06-19 12:35:25 +0200

    Build: Update to Gradle 8.1, Loom 1.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         maven("https://repo.essential.gg/repository/maven-public")
     }
     plugins {
-        val egtVersion = "0.1.15"
+        val egtVersion = "0.2.0"
         id("gg.essential.multi-version.root") version egtVersion
         id("gg.essential.multi-version.api-validation") version egtVersion
     }
