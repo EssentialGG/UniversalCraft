dd9fa6931115d5e28d64fce007d7155e535e47df
Jonas Herzig <jonas@spark-squared.com>
2023-06-20 10:46:57 +0200

    Build: Update to Gradle 8.1.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         maven("https://repo.essential.gg/repository/maven-public")
     }
     plugins {
-        val egtVersion = "0.2.0"
+        val egtVersion = "0.2.1"
         id("gg.essential.multi-version.root") version egtVersion
         id("gg.essential.multi-version.api-validation") version egtVersion
     }
