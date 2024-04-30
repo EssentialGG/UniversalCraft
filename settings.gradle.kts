34ea642e1902639790f8e5309b8378aef9d1341f
DJtheRedstoner <52044242+DJtheRedstoner@users.noreply.github.com>
2024-04-30 16:56:39 -0400

    Build: Update to EGT 0.5.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         maven("https://repo.essential.gg/repository/maven-public")
     }
     plugins {
-        val egtVersion = "0.4.1"
+        val egtVersion = "0.5.0"
         id("gg.essential.multi-version.root") version egtVersion
         id("gg.essential.multi-version.api-validation") version egtVersion
     }
